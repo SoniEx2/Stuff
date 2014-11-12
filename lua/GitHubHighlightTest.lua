ab914ad84ff6acb12703a6d32de644946e2d3e67
SoniEx2 <EnderMoneyMod@gmail.com>
2014-11-12 17:41:35 -0200

    Update GitHubHighlightTest.lua

--- lua/GitHubHighlightTest.lua
+++ lua/GitHubHighlightTest.lua
@@ -1,1 +1,34 @@
+--[[
+long comment
+]]
 
+--[==[
+level 2 long comment
+]==]
+
+s = "string"
+
+ls = [[
+long string - why does this show up as a comment on the editor?
+]]
+
+l2ls = [==[
+level 2 long string - also shows up as comment on the editor
+]==]
+
+mls = "multi\
+line\
+string" -- doesn't show up properly on the editor
+
+fcall(s)
+fcall_s"string"
+fcall_t{s}
+fcall_ls[[
+long string - also shows up as comment on the editor
+]]
+fcall_l2ls[==[
+level 2 long string - also shows up as comment on the editor
+]==]
+fcall_mls"multi\
+line\
+string" -- doesn't show up properly on the editor
