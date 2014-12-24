19e72e4e0672292c9fd853e21c4953e32023a479
SoniEx2 <endermoneymod@gmail.com>
2014-12-24 20:03:39 -0200

    Fix bugs and screw 5.0 compat

--- lua/String.lua
+++ lua/String.lua
@@ -1,19 +1,9 @@
 local squote = "'"
 local dquote = '"'
 
--- escape "sequences"
-local escapeSequences = {
-  a = '\a',
-  b = '\b',
-  f = '\f',
-  r = '\r',
-  n = '\n',
-  t = '\t',
-  v = '\v',
-  ['"'] = '"',
-  ["'"] = "'",
-  ['\\'] = '\\'
-}
+local function chartopad(c)
+  return string.format("\\%03d", string.byte(c))
+end
 
 local pads = {
   z = "\\z",
@@ -27,47 +17,55 @@ local pads = {
   ['6'] = '\\6',
   ['7'] = '\\7',
   ['8'] = '\\8',
-  ['9'] = '\\9'
+  ['9'] = '\\9',
+
+  -- remap escape sequences
+  a = chartopad('\a'),
+  b = chartopad('\b'),
+  f = chartopad('\f'),
+  r = chartopad('\r'),
+  n = chartopad('\n'),
+  t = chartopad('\t'),
+  v = chartopad('\v'),
+  ['"'] = chartopad('\"'),
+  ["'"] = chartopad('\''),
+['\\'] = chartopad('\\')
+}
+
+local numbers = {
+  ['0'] = true,
+  ['1'] = true,
+  ['2'] = true,
+  ['3'] = true,
+  ['4'] = true,
+  ['5'] = true,
+  ['6'] = true,
+  ['7'] = true,
+  ['8'] = true,
+  ['9'] = true
 }
 
 setmetatable(pads, {
-    __index = function(t,k)
-      return "\\v" .. k .. "/"
-    end
+    __index = function(t,k) error("invalid escape sequence near '\\" .. k .. "'", 3) end
   })
 
 local findpairs
 do
-  --[[
-  local function _findnext_util(a, b, ...)
-    return b, coroutine.yield(a, b, ...)
-  end
-  local function _findnext(stringandpattern, index)
-    while true do
-      index = _findnext_util(string.find(stringandpattern[1], stringandpattern[2], index + 1))
-    end
-  end
-  function findpairs(string, pattern)
-    return coroutine.wrap(_findnext), {string, pattern}, 0
-  end
-  --]]
-  -- [[
-  -- alternate (stateless) findpairs (untested)
-  local function _findnext(stringandpattern, index)
+  local function _findnext(flags, index)
     -- only branch if index = 0
     if index > 0 then
       -- this should always give a match,
       -- as we're starting from the same index as the returned match
-      local x,y = string.find(stringandpattern[1], stringandpattern[2], index)
-      return string.find(stringandpattern[1], stringandpattern[2], y+1)
+      -- TODO: test %f >.>
+      local x,y = string.find(flags[1], flags[2], index, flags[3])
+      return string.find(flags[1], flags[2], y+1, flags[3])
     else
-      return string.find(stringandpattern[1], stringandpattern[2], index + 1)
+      return string.find(flags[1], flags[2], index + 1, flags[3])
     end
   end
-  function findpairs(string, pattern)
-    return _findnext, {string, pattern}, 0
+  function findpairs(str, pat, raw)
+    return _findnext, {str, pat, raw}, 0
   end
-  --]]
 end
 
 -- Parse a string like it's a Lua 5.2 string.
@@ -80,37 +78,35 @@ local function parseString52(s)
   -- remove quotes
   local str = string.sub(s, 2, -2)
 
-  -- TODO check for unescaped quotes
-
   -- replace "normal" escapes with a padded escape
-  str = string.gsub(str, "\\(.)", function(c)
-      -- swap startChar with some invalid escape
-      if c == startChar then
-        c = "m"
-        -- swap the invalid escape with startChar
-      elseif c == "m" then
-        c = startChar
-      end
-      return pads[c]
-    end)
+  str = string.gsub(str, "\\(.)", pads)
 
-  -- check for a padded escape for startChar - remember this is actually our invalid escape
-  assert(not string.find(str, "\\v" .. startChar .. "/"), "invalid escape sequence near '\\m'")
-
-  -- then check for non-escaped startChar
+  -- check for non-escaped startChar
   assert(not string.find(str, startChar), "unfinished string")
 
-  -- pad 1-digit numerical escapes
-  str = string.gsub(str, "\\([0-9])[^0-9]", "\\00%1")
-
-  -- pad 2-digit numerical escapes
-  str = string.gsub(str, "\\([0-9][0-9])[^0-9]", "\\0%1")
+  -- pad numerical escapes
+  str = string.gsub(str, "\\([0-9])(.?)(.?)", function(a, b, c)
+      local x = a
+      -- swap b and c if #b == 0; this is to avoid UB:
+      -- _in theory_ `c` could match but not `b`, this solves
+      -- that problem. uncomment if you know what you're doing.
+      if #b == 0 then b, c = c, b end
+      if numbers[b] then
+        x, b = x .. b, ""
+        if numbers[c] then
+          x, c = x .. c, ""
+        end
+      end
+      local temp1 = ("0"):rep(3 - #x)
+      return "\\" .. temp1 .. x .. b .. c
+    end)
 
   local t = {}
   local i = 1
   local last = 1
   -- split on \z
-  for from, to in findpairs(str, "\\z") do
+  -- we can look for "\z" directly because we already escaped everything else
+  for from, to in findpairs(str, "\\z", true) do
     t[i] = string.sub(str, last, from - 1)
     last = to+1
     i = i + 1
@@ -120,17 +116,15 @@ local function parseString52(s)
   -- parse results
   local nt = {}
   for x,y in ipairs(t) do
-    nt[x] = string.gsub(y, "\\(([vx0-9])((.).))",
+    nt[x] = string.gsub(y, "\\(([x0-9])((.).))",
       function(a,b,c,d)
-        if b == "v" then
-          return escapeSequences[d] or (d == "m" and startChar or assert(false, "invalid escape sequence near '\\" .. d .. "'"))
-        elseif b == "x" then
-          local n = tonumber(c, 16)
-          assert(n, "hexadecimal digit expected near '\\x" .. c .. "'")
-          return string.char(n)
-        else
+        if b ~= "x" then
           local n = tonumber(a)
           assert(n < 256, "decimal escape too large near '\\" .. a .. "'")
+          return string.char(n)
+        else
+          local n = tonumber(c, 16)
+          assert(n, "hexadecimal digit expected near '\\x" .. c .. "'")
           return string.char(n)
         end
       end)
@@ -146,8 +140,6 @@ end
 -- "tests"
 -- TODO add more
 -- also add automatic checks
--- syntax error in Lua 5.0 :/ uncomment to enable
---[[
 if _VERSION == "Lua 5.2" and not ... then
   local t = {
     [=["\""]=],
@@ -155,7 +147,8 @@ if _VERSION == "Lua 5.2" and not ... then
     [=["v""]=],
     [=[""/"]=],
     [=["\v"/"]=],
-    [=["\m"]=]
+    [=["\m"]=],
+    [=["\32"]=],
   }
   for _, str in ipairs(t) do
     local s, m = pcall(parseString52, str)
@@ -168,7 +161,6 @@ if _VERSION == "Lua 5.2" and not ... then
 elseif not ... then
   print("Tests require Lua 5.2")
 end
---]]
 
 return {
   parse52 = parseString52,
