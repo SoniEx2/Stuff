444bc7b0920dbf60320fcb2d2dd48ea63721c793
SoniEx2 <endermoneymod@gmail.com>
2015-04-04 13:51:18 -0300

    Make Stack.lua a module

--- lua/Stack.lua
+++ lua/Stack.lua
@@ -1,10 +1,13 @@
 -- Stacks. Because why not!
+local stack = {}
 
 -- Stack-based table.unpack
-local function unpack(t, i, j)
+function stack.unpack(t, i, j)
   local function unpack_impl(t, i, j, ...)
     if i == j then return t[j], ... end
     return unpack_impl(t, i, j - 1, t[j], ...)
   end
   return unpack_impl(t, i or 1, j or #t)
 end
+
+return stack
